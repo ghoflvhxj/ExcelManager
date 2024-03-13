4c8f8e8f4cc2356d0a09a7579e99d58f6d440a88
문경현 <mkh2022@action2quare.corp>
2024-03-13 13:59:59 +0900

    GGG도 리소스 검사가 적용되도록 수정

--- Source/DataTable/AnvilTeamTable.cs
+++ Source/DataTable/AnvilTeamTable.cs
@@ -177,11 +177,8 @@ namespace TestWPF
         }
 
         private static Dictionary<string, byte> enumMap;
-        private static int rowReadCounter;
         public static void MakeBinaryFiles(List<string> excelFilePath/*, Func<float, bool> OnLoadLatestCompleted, Func<float, bool> OnRowRead*/)
         {
-            rowReadCounter = 0;
-
             if (excelFilePath.Count == 0)
             {
                 return;
@@ -360,7 +357,8 @@ namespace TestWPF
                                 {
                                     Int32 value = 0;
 
-                                    if (cellToString.Any(IsInValidInteger))
+                                    string signRemovedString = cellToString[0] == '-' ? cellToString[1..] : cellToString;
+                                    if (signRemovedString.Any(IsInValidInteger))
                                     {
                                         GetDataCheckMessage(ref cellToString, ref dataCheckMessage, columnHeader, row, "0");
                                     }
@@ -390,7 +388,8 @@ namespace TestWPF
                                 {
                                     Int64 value = 0;
 
-                                    if (cellToString.Any(char.IsLetter))
+                                    string signRemovedString = cellToString[0] == '-' ? cellToString[1..] : cellToString;
+                                    if (signRemovedString.Any(IsInValidInteger))
                                     {
                                         GetDataCheckMessage(ref cellToString, ref dataCheckMessage, columnHeader, row, "0");
                                     }
@@ -429,7 +428,8 @@ namespace TestWPF
                                 {
                                     byte value = 0;
 
-                                    if (cellToString.Any(char.IsLetter))
+                                    string signRemovedString = cellToString[0] == '-' ? cellToString[1..] : cellToString;
+                                    if (signRemovedString.Any(IsInValidInteger))
                                     {
                                         GetDataCheckMessage(ref cellToString, ref dataCheckMessage, columnHeader, row, "0");
                                     }
@@ -446,7 +446,8 @@ namespace TestWPF
                                 {
                                     Int16 value = 0;
 
-                                    if (cellToString.Any(char.IsLetter))
+                                    string signRemovedString = cellToString[0] == '-' ? cellToString[1..] : cellToString;
+                                    if (signRemovedString.Any(IsInValidInteger))
                                     {
                                         GetDataCheckMessage(ref cellToString, ref dataCheckMessage, columnHeader, row, "0");
                                     }
@@ -463,9 +464,10 @@ namespace TestWPF
                                 {
                                     float value = 0;
 
-                                    if (cellToString.Any(IsInValidFloat))
+                                    string signRemovedString = cellToString[0] == '-' ? cellToString[1..] : cellToString;
+                                    if (signRemovedString.Any(IsInValidFloat))
                                     {
-                                        GetDataCheckMessage(ref cellToString, ref dataCheckMessage, columnHeader, row, "0");
+                                        GetDataCheckMessage(ref signRemovedString, ref dataCheckMessage, columnHeader, row, "0");
                                     }
                                     else
                                     {
@@ -480,7 +482,8 @@ namespace TestWPF
                                 {
                                     double value = 0;
 
-                                    if (cellToString.Any(IsInValidFloat))
+                                    string signRemovedString = cellToString[0] == '-' ? cellToString[1..] : cellToString;
+                                    if (signRemovedString.Any(IsInValidFloat))
                                     {
                                         GetDataCheckMessage(ref cellToString, ref dataCheckMessage, columnHeader, row, "0");
                                     }
@@ -576,6 +579,222 @@ namespace TestWPF
         private void GetDataCheckMessage(ref string data, ref string dataCheckMessage, AnvilColumnHeader columnHeader, int row, string defaultValue)
         {
             dataCheckMessage += "[" + row + ", " + columnHeader.Name + "] 의 " + " 데이터(" + data + ")와 타입(" + Enum.GetName(typeof(EDataType), columnHeader.DataType) + ")이 다릅니다.\r\n";
+        }
+
+        public static void FixResources(List<string> excelFilePath)
+        {
+            // 엑셀 파일을 읽는다
+            List<AnvilTeamTable> LoadedGameDataTables = new();
+            for (int i = 0; i < excelFilePath.Count; ++i)
+            {
+                string path = excelFilePath[i];
+                AnvilTeamTable table = (AnvilTeamTable)GameDataTable.GetTableByPath(path);
+                if (table == null)
+                {
+                    continue;
+                }
+
+                if (table.Load(((App)App.Current).ExcelLoader, table.DataArray == null))
+                {
+                    LoadedGameDataTables.Add(table);
+                }
+            }
+
+            foreach (AnvilTeamTable anvilTeamTable in LoadedGameDataTables)
+            {
+                anvilTeamTable.FixResource();
+            }
+        }
+
+        public void FixResource()
+        {
+            const int maxWorkers = 4;
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(maxWorkers, maxWorkers);
+
+            List<EventWaitHandle> threadEvents = new List<EventWaitHandle>(ResourceColums.Count);
+            foreach (var pair in ResourceColums)
+            {
+                EResourcePathType resourcePathType = pair.Key;
+                int col = pair.Value.ColumnIndex;
+
+                // 멀티스레드지만 스레드가 동일한 원소에 접근하지 않아 내부에서 락을 안잡아도 됨
+                ThreadPool.QueueUserWorkItem(CheckResourceData, new ResourceCheckInfo() { ColumnIndex = col, ColumnName = pair.Value.Name, ExcelPath = FilePath, resourcePathType = pair.Key, RowCount = RowCount });
+            }
+
+
+            //Load(((App)App.Current).ExcelLoader, DataArray == null);
+
+            //const int maxWorkers = 4;
+            //ThreadPool.SetMinThreads(1, 1);
+            //ThreadPool.SetMaxThreads(maxWorkers, maxWorkers);
+
+            //List<EventWaitHandle> threadEvents = new List<EventWaitHandle>(ResourceColums.Count);
+            //foreach (var pair in ResourceColums)
+            //{
+            //    EResourcePathType resourcePathType = pair.Key;
+            //    int col = pair.Value.ColumnIndex;
+
+            //    // 멀티스레드지만 스레드가 동일한 원소에 접근하지 않아 내부에서 락을 안잡아도 됨
+            //    ThreadPool.QueueUserWorkItem(CheckResourceData, new ResourceCheckInfo() { ColumnIndex = col, ColumnName = pair.Value.Name, ExcelPath = FilePath, resourcePathType = pair.Key, RowCount = RowCount });
+            //}
+        }
+
+        private void CheckResourceData(object obj)
+        {
+            ResourceCheckInfo resourceCheckInfo = obj as ResourceCheckInfo;
+            if (resourceCheckInfo == null)
+            {
+                return;
+            }
+
+            string colName = resourceCheckInfo.ColumnName;
+            string excelPath = resourceCheckInfo.ExcelPath;
+            int columnIndex = resourceCheckInfo.ColumnIndex;
+
+            for (int row = (int)EColumnHeaderElement.Count + 1; row <= resourceCheckInfo.RowCount; ++row)
+            {
+                object cellObject = GameDataTable.GameDataTableMap[excelPath].DataArray[row, columnIndex];
+                if (cellObject == null)
+                {
+                    continue;
+                }
+
+                string originCellValue = cellObject.ToString();
+                switch (resourceCheckInfo.resourcePathType)
+                {
+                    case EResourcePathType.FileName:
+                        {
+                            string fileName = originCellValue;
+                            if (MainWindow.allFileName.ContainsKey(fileName) == false)
+                            {
+                                string message = GetRowColumnString(excelPath, row, colName, fileName);
+                                string fileNameAsKey = Utility.NameAsKey(fileName);
+                                if (MainWindow.allFileNameAsKey.ContainsKey(fileNameAsKey))
+                                {
+                                    message += GetCheckResultAsMessage(ECheckResult.InvalidFileName, fileName, fileNameAsKey);
+                                    //GameDataTable.GameDataTableMap[excelPath].DataArray[row, columnIndex] = MainWindow.allFileNameAsKey[fileNameAsKey];
+                                }
+                                else
+                                {
+                                    message += GetCheckResultAsMessage(ECheckResult.NotExistFile, fileName);
+                                }
+                            }
+                        }
+                        break;
+                    case EResourcePathType.Path:
+                        {
+                            string resourcePath = originCellValue;
+                            string fileName = Path.GetFileName(resourcePath);
+                            if (resourcePath[0] == '/')
+                            {
+                                resourcePath = resourcePath.Substring(1);
+                            }
+
+                            string[] directoryNames = Path.GetDirectoryName(resourcePath).Split('\\');
+                            if (directoryNames.Length > 0)
+                            {
+                                int offset = 0;
+                                if (directoryNames[0] == "Game")
+                                {
+                                    ++offset;
+                                }
+
+                                string message = "";
+                                // 폴더 이름 검사
+                                for (int i = directoryNames.Length - 1; i >= offset; --i)
+                                {
+                                    if (MainWindow.allDirectoryName.ContainsKey(directoryNames[i]))
+                                    {
+                                        continue;
+                                    }
+
+                                    message = (message.Length != 0) ? message : GetRowColumnString(excelPath, row, colName, resourcePath);
+
+                                    string directoryNameAsKey = Utility.NameAsKey(directoryNames[i]);
+                                    if (MainWindow.allDirectoryActualNames.ContainsKey(directoryNameAsKey))
+                                    {
+                                        if (i > 1 && MainWindow.allDirectoryParentNames[directoryNameAsKey].Contains(Utility.NameAsKey(directoryNames[i - 1])))
+                                        {
+                                            message += GetCheckResultAsMessage(ECheckResult.InvalidDirectoryName, directoryNames[i], directoryNameAsKey);
+                                            string onlyDirectroy = originCellValue.Substring(0, originCellValue.LastIndexOf('/') + 1);
+                                            //GameDataTable.GameDataTableMap[excelPath].DataArray[row, columnIndex] = originCellValue = onlyDirectroy.Replace(directoryNames[i], MainWindow.allDirectoryActualNames[directoryNameAsKey]) + fileName;
+                                        }
+                                        else
+                                        {
+                                            message += GetCheckResultAsMessage(ECheckResult.NotExistDirectory, directoryNames[i]);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        message += GetCheckResultAsMessage(ECheckResult.NotExistDirectory, directoryNames[i]);
+                                    }
+                                }
+                                // 파일 이름 검사
+                                string fileNameAsKey = Utility.NameAsKey(fileName);
+                                bool bWrongLetter = MainWindow.allFileName.ContainsKey(fileName) == false;
+                                bool bExist = MainWindow.allFileNameAsKey.ContainsKey(fileNameAsKey);
+                                if (bWrongLetter && bExist)
+                                {
+                                    message = (message.Length != 0) ? message : GetRowColumnString(excelPath, row, colName, resourcePath);
+                                    message += GetCheckResultAsMessage(ECheckResult.InvalidFileName, fileName, fileNameAsKey);
+
+                                    string onlyDirectroy = originCellValue.Substring(0, originCellValue.LastIndexOf('/') + 1);
+                                    //GameDataTable.GameDataTableMap[excelPath].DataArray[row, columnIndex] = onlyDirectroy + MainWindow.allFileNameAsKey[fileNameAsKey];
+                                }
+                                else if (bExist == false)
+                                {
+                                    message = (message.Length != 0) ? message : GetRowColumnString(excelPath, row, colName, resourcePath);
+                                    message += GetCheckResultAsMessage(ECheckResult.NotExistFile, fileName);
+                                }
+
+                                if (message.Length > 0)
+                                {
+                                    Utility.Log(message, LogType.Warning);
+                                }
+                            }
+                        }
+                        break;
+                }
+            }
+        }
+
+
+        private string GetRowColumnString(string excelPathFileName, int row, int col, string data)
+        {
+            return Path.GetFileName(excelPathFileName) + "[" + (row + (int)EColumnHeaderElement.Count) + "," + col + "]: " + data + "\r\n";
+        }
+
+        private string GetRowColumnString(string excelPathFileName, int row, string colName, string data)
+        {
+            return Path.GetFileName(excelPathFileName) + "[" + row + "," + colName + "]: " + data + "\r\n";
+        }
+
+        private string GetModifedMessage()
+        {
+            return "└ 수정되었습니다. \r\n";
+        }
+
+        private string GetCheckResultAsMessage(ECheckResult result, string str, string strAsKey = "")
+        {
+            string message = "";
+            switch (result)
+            {
+                case ECheckResult.InvalidDirectoryName:
+                    message = " 제안사항: 폴더의 대소문자 점검, " + "엑셀에 적힌 폴더이름:" + str + " ≠ " + "실제 폴더이름:" + MainWindow.allDirectoryActualNames[strAsKey];
+                    break;
+                case ECheckResult.InvalidFileName:
+                    message = " 제안사항: 파일의 대소문자 점검, " + "엑셀에 적힌 파일이름:" + str + " ≠ " + "실제 파일이름:" + MainWindow.allFileNameAsKey[strAsKey];
+                    break;
+                case ECheckResult.NotExistDirectory:
+                    message = " 존재하지 않는 디렉토리 입니다 " + str;
+                    break;
+                case ECheckResult.NotExistFile:
+                    message = " 존재하지 않는 파일입니다 " + str;
+                    break;
+            }
+
+            return message;
         }
     }
 }
